f2f0026133eca77c9c289819aa95cd1ff869d4cd
kieranMifsud18 <32700593+kieranMifsud18@users.noreply.github.com>
2017-12-08 03:25:27 -0800

    third

---
+++ Assignment/Assets/Scripts/Paddle.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour{
+
+    private Ball myBall;
+
+    void Start()
+    {
+        myBall = GameObject.FindObjectOfType<Ball>();
+    }
+
+    void Update()
+    {
+        float mousePosInUnits = (Input.mousePosition.X / Screen.width * 13f) - 6.5;
+
+
+        float mousePos = Mathf.Clamp(myBall.transform.position.y, 0);
+
+        this.transform.position = paddlePostition;
+    }
+}
